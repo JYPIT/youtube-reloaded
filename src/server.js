02f68e2acbf0d055d49af13252ff237c63c594b3
JYPIT <jypit@naver.com>
2022-07-19 12:47:43 +0900

    router 구성

--- src/server.js
+++ src/server.js
@@ -1,20 +1,18 @@
 import express from "express";
-import { reset } from "nodemon";
+import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import videoRouter from "./routers/videoRouter";
+import userRouter from "./routers/userRouter";
 
 const PORT = 4000;
 
 const app = express();
+const logger = morgan("dev");
+app.use(logger);
 
-const logger = (req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  next();
-};
-
-const handleHome = (req, res) => {
-  return res.send("Final Rounge");
-};
-
-app.get("/", logger, handleHome);
+app.use("/", globalRouter);
+app.use("/users", userRouter);
+app.use("/videos", videoRouter);
 
 const handleListening = () => console.log(`✔️ Server listening on port http://localhost:${PORT} 🚀`);
 
