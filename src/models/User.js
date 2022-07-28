bb6960fe20587472c38a8ef0db43755e6f8c50fa
JYPIT <jypit@naver.com>
2022-07-28 11:57:13 +0900

    edit-profile 생성

--- src/models/User.js
+++ src/models/User.js
@@ -3,16 +3,16 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
+  avatarUrl: String,
+  socialOnly: { type: Boolean, default: false },
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+  password: { type: String, required: false },
   location: String,
 });
 
 userSchema.pre("save", async function () {
-  console.log("User password:", this.password);
   this.password = await bcrypt.hash(this.password, 5);
-  console.log("Hashed password", this.password);
 });
 
 const User = mongoose.model("User", userSchema);
